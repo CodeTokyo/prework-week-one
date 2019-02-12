3a87690a5b21efd307e2d0eccd404b9a3ddbda85
CodeTokyo <b-man225@hotmail.com>
2019-02-11 20:28:17 -0600

    corrected_expressions.rb

---
+++ evaluated_expressions.rb
@@ -0,0 +1,10 @@
+#These are my answers
+puts 3 + 3 + 3 * 10 == 36
+puts (2 * 4) / 2 - 4 == 0
+puts "hello" * 3 == "hellohellohello"
+puts "hello" + "3" == "hello3"
+puts !true == !true
+puts "false" == "false"
+puts true && 2 + 2 == 4
+puts false || true
+puts false || true && 3 == 3
