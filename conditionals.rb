ad604d75d6eb80991571f75135469dc43c6087e2
CodeTokyo <b-man225@hotmail.com>
2019-02-12 23:25:19 -0600

    Conditionals.rb

---
+++ conditionals.rb
@@ -0,0 +1,10 @@
+def enter_number
+  puts "Please enter the number 1 or 2"
+  number = gets.chomp.to_i
+
+  if number == 2 || number == 1
+    puts "Thank you for entering a #{number.to_s}!"
+  else
+    puts "That is incorrect"
+  end
+end
